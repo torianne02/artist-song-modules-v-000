aa2b533c4bfb74e4ba892b67935244cba4c0d0cc
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-17 18:54:47 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,5 @@ require 'pry'
 # require_relative '../lib/concerns/findable'
  require_relative '../lib/concerns/memorable'
 # require_relative '../lib/concerns/paramable'
-binding.pry
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'

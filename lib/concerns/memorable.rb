1d49b3fa0f565ca1d4995325577305a491551a8b
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-17 19:16:41 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,14 @@
 module Memorable
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
+    def count
+      self.all.count
+    end
   end
+  module InstanceMethods
+    def initialize
+      self.class.all << self
 end
